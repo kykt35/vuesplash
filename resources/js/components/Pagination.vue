289537479b236b06b7b5cb54f0e9dabd311d4e7d
kykt35 <kykt35@gmail.com>
2019-02-20 15:08:35 +0900

    fix typo

--- resources/js/components/Pagination.vue
+++ resources/js/components/Pagination.vue
@@ -4,7 +4,7 @@
             v-if="! isFirstPage"
             :to="`/?page=${currentPage - 1}`"
             class="button"
-        >$laquo; prev</RouterLink>
+        >&laquo; prev</RouterLink>
         <RouterLink 
             v-if="! isLastPage"
             :to="`/?page=${currentPage + 1}`"
